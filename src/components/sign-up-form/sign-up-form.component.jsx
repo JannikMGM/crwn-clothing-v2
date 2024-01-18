6e4efbc80a9fc658222cffb8a892126358fed382
JannikMGM <jannik.kessler@mgm-tp.com>
2024-01-18 14:27:15 +0100

    created authentication page component alongside sign in form

--- src/components/sign-up-form/sign-up-form.component.jsx
+++ src/components/sign-up-form/sign-up-form.component.jsx
@@ -15,7 +15,7 @@ const defaultFormFields = {
 }
 
 const SignUpForm = () => {
-    const [formFields, setFormFields] = useState(defaultFormFields);
+    const [ formFields, setFormFields ] = useState(defaultFormFields);
     const { displayName, email, password, confirmPassword } = formFields;
 
     console.log(formFields)
@@ -51,7 +51,7 @@ const SignUpForm = () => {
     const handleChange = (event) => {
         const { name, value } = event.target;
 
-        setFormFields({ ...formFields, [name]: value} );
+        setFormFields({ ...formFields, [name]: value});
     };
 
     return (
