83bc5b1012c164b5ab7be4212595f332aa0b225b
JannikMGM <jannik.kessler@mgm-tp.com>
2024-01-19 09:03:58 +0100

    added user context component and centralized context manipulation through onAuthStateChangedListener

--- src/components/sign-up-form/sign-up-form.component.jsx
+++ src/components/sign-up-form/sign-up-form.component.jsx
@@ -18,8 +18,6 @@ const SignUpForm = () => {
     const [ formFields, setFormFields ] = useState(defaultFormFields);
     const { displayName, email, password, confirmPassword } = formFields;
 
-    console.log(formFields)
-
     const resetFormFields = () => {
         setFormFields(defaultFormFields);
     }
@@ -36,7 +34,6 @@ const SignUpForm = () => {
             const { user } = await createAuthUserWithEmailAndPassword(email, password);
 
             await createUserDocumentFromAuth(user, { displayName });
-
             resetFormFields();
         } catch(error) {
             if (error.code === 'auth/email-already-in-use') {
