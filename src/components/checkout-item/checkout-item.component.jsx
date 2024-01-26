8d8a64a47aa0e8b74ebd732005d379449c795c4b
JannikMGM <jannik.kessler@mgm-tp.com>
2024-01-26 14:07:32 +0100

    migrating from react context to redux state management

--- src/components/checkout-item/checkout-item.component.jsx
+++ src/components/checkout-item/checkout-item.component.jsx
@@ -1,6 +1,7 @@
-import { useContext } from 'react';
+import { useDispatch, useSelector } from 'react-redux'; 
 
-import { CartContext } from '../../contexts/cart.context.jsx';
+import { clearItemFromCart, addItemToCart, removeItemFromCart } from '../../store/cart/cart.action.js';
+import { selectCartItems } from '../../store/cart/cart.selector.js';
 
 import { 
     CheckoutItemContainer, 
@@ -12,13 +13,14 @@ import {
 } from './checkout-item.styles.jsx';
 
 const CheckoutItem = ({ cartItem }) => {
-    const { clearItemFromCart, addItemToCart, removeItemFromCart } = useContext(CartContext);
+    const dispatch = useDispatch();
+    const cartItems = useSelector(selectCartItems);
     const { name, imageUrl, price, quantity } = cartItem;
 
-    const addItemHandler = () => addItemToCart(cartItem);
-    const removeItemHandler = () => removeItemFromCart(cartItem);
+    const addItemHandler = () => dispatch(addItemToCart(cartItems, cartItem));
+    const removeItemHandler = () => dispatch(removeItemFromCart(cartItems, cartItem));
 
-    const clearItemHandler = () => clearItemFromCart(cartItem);
+    const clearItemHandler = () => dispatch(clearItemFromCart(cartItems, cartItem));
 
     return (
         <CheckoutItemContainer>
---
+++ src/store/store.js
@@ -0,0 +1,25 @@
+import { compose, createStore, applyMiddleware } from 'redux';
+/* import logger from 'redux-logger'; */
+
+import { rootReducer } from './root-reducer';
+import { composeWithDevTools } from '@redux-devtools/extension';
+
+const loggerMiddleware = (store) => (next) => (action) => {
+    if(!action.type) {
+        return next(action);
+    }
+
+    console.log('type: ', action.type)
+    console.log('payload: ', action.payload)
+    console.log('currentState: ', store.getState())
+
+    next(action);
+
+    console.log('next state: ', store.getState());
+}
+
+const middleWares = [loggerMiddleware];
+
+const composedEnhancers = compose(applyMiddleware(...middleWares));
+
+export const store = createStore(rootReducer, /* composeWithDevTools(applyMiddleware(...middleWares)) */composedEnhancers);
